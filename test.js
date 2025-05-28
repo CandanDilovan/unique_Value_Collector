078e49d1a80f36d7def9487226f13ad8f5008156
unknown <candandilovan@gmail.com>
2025-05-28 16:37:29 +0200

    test

--- test.js
+++ test.js
@@ -2,11 +2,7 @@ console.log("loadeddddddddddddddddddddddddddddddddddddddddddddddddd")
 console.log("typeof grist:", typeof grist);
 console.log("grist.ready exists:", typeof grist.ready);
 
-setTimeout(() => {
-  grist.ready(() => {
-    console.log("🎉 INSIDE grist.ready");
-    grist.setHeight(300);
-  });
-}, 100);
+grist.ready()
+
 
 console.log("laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
