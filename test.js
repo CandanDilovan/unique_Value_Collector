74c27120e32599964390ca193b1b840e19d54466
unknown <candandilovan@gmail.com>
2025-05-28 16:26:03 +0200

    test

--- test.js
+++ test.js
@@ -2,13 +2,9 @@ console.log("loadeddddddddddddddddddddddddddddddddddddddddddddddddd")
 console.log("typeof grist:", typeof grist);
 console.log("grist.ready exists:", typeof grist.ready);
 
-if (typeof grist !== undefined){
-
-    grist.ready(() => {
-        console.log('testtttttttttttttttttttttttttttttttttttttttttttttttttttt');
-        grist.setTitle("readyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
-    });
-}
-else {
-    console.log("nope sorry bruvs")
-}
+setTimeout(() => {
+  grist.ready(() => {
+    console.log("🎉 INSIDE grist.ready");
+    grist.setHeight(300);
+  });
+}, 0);
