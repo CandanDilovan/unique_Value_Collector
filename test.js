9832ee7d2d0b8ff8f468b82db7c46c1974dfaa55
unknown <candandilovan@gmail.com>
2025-05-28 16:15:43 +0200

    test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 console.log("loadeddddddddddddddddddddddddddddddddddddddddddddddddd")
-
+console.log("typeof grist:", typeof grist);
+console.log("grist.ready exists:", typeof grist.ready);
 
 if (typeof grist !== undefined){
 
