fd5c297fe346bb837f978ad150597cb37c122b57
unknown <candandilovan@gmail.com>
2025-05-30 10:50:32 +0200

    test

--- test.js
+++ test.js
@@ -5,7 +5,8 @@ console.log("grist.ready exists:", typeof grist.ready);
 
 async function start(){
     grist.ready();
-    let tables = await grist.docApi.getTableNames();
+    // let tables = await grist.getTableNames();
+    console.log("docApi methods:", Object.keys(grist.docApi));
     console.log("testtttttttttttttttttttttttttt");
 }
 
