bc8fb4cb7379417c47af933cdd7087592272d880
unknown <candandilovan@gmail.com>
2025-06-03 14:51:03 +0200

    almost here

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ async function py_Start(src, dst){
   py.globals.set("src", py.toPy(src));
   let test = await py.runPythonAsync(`test(src)`);
   console.log("result of the test: ", test)
-  grist.addRecord(dst, test)
+  grist.docApi.AddOrUpdateRecord(dst, test)
 
 }
 
