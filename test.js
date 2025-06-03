debfa2a78554544cbee61b1f30359a350556d6e3
unknown <candandilovan@gmail.com>
2025-06-03 18:37:08 +0200

    almost here

--- test.js
+++ test.js
@@ -25,7 +25,7 @@ async function py_Start(src, dst){
     }
   console.log(dst, src, src.textContent)
 
-  let action = ["ReplaceTableData", dst.value , colData];
+  let action = ["ReplaceTableData", dst , colData];
   // console.log("ACTIOOOOOOOOOOOOOONNNNNNNNNNNNNSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS", JSON.stringify(action)) 
   await grist.docApi.applyUserActions([action]);
 
