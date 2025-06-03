9302caace6b1e1f8cbd6089acf3d89cd704c6ee7
unknown <candandilovan@gmail.com>
2025-06-03 14:35:38 +0200

    almost here

--- unique_collector.py
+++ unique_collector.py
@@ -6,8 +6,8 @@ def test(src):
     for x in range(len(table.columns)):
         unique_lst = set()
         for y in range(table.index):
-            if table.at[table.index[y], table.columns[x]] in unique_lst:
-                table.at[table.index[y], table.columns[x]] = None
+            if table.at[y, x] in unique_lst:
+                table.at[y, x] = None
             else:
-                unique_lst.add(table.at[table.index[y], table.columns[x]])
+                unique_lst.add(table.at[y, x])
     return table
