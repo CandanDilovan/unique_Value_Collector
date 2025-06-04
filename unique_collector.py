575f87ae244b616f91872c9a31b60fa52f8cb517
unknown <candandilovan@gmail.com>
2025-06-04 09:54:31 +0200

    almost here

--- unique_collector.py
+++ unique_collector.py
@@ -4,21 +4,13 @@ import pandas as pd
 def test(src):
     col_to_drop = ["id", "gristHelper_Display", "manualSort"]
     table = pd.DataFrame(src)
+    table = table.drop(columns=[col for col in col_to_drop if col in table.columns])
+
     for x in range(len(table.columns)):
-        if check_cols(col_to_drop, table.columns[x]):
-            table.drop(table.columns[x], axis=1, inplace=True)
-        else:
-            unique_lst = set()
-            for y in range(len(table.index)):
-                if table.at[table.index[y], table.columns[x]] in unique_lst:
-                    table.at[table.index[y], table.columns[x]] = None
-                else:
-                    unique_lst.add(table.at[table.index[y], table.columns[x]])
+        unique_lst = set()
+        for y in range(len(table.index)):
+            if table.at[table.index[y], table.columns[x]] in unique_lst:
+                table.at[table.index[y], table.columns[x]] = None
+            else:
+                unique_lst.add(table.at[table.index[y], table.columns[x]])
     return table
-
-
-def check_cols(lst: list, df: pd.DataFrame) -> bool:
-    for element in lst:
-        if element in df:
-            return True
-    return False
