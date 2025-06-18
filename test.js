cae809530bd985c5a600ea968e0615c2683bdf1a
unknown <candandilovan@gmail.com>
2025-06-18 17:11:41 +0200

    nuthin

--- test.js
+++ test.js
@@ -47,8 +47,8 @@ async function py_Start(src, dst, dstcol)
         for (let x = 0; x < records.length; x++) 
         {
           let dsttable = await grist.docApi.fetchTable(dst.value)
-          console.log(dsttable[dst.text][x])
-          if (dsttable[dst.text][x] !== records[x])
+          console.log(dsttable[dstcol][x])
+          if (dsttable[dstcol][x] !== records[x])
             await grist.docApi.applyUserActions([["AddRecord", dst.text, null, records[x]]]);
           
           const stepProgress = 80 + ((x + 1) / records.length) * 20;
