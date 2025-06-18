7834b9bcf1c3d9eb75e870cf437347506acc2a3e
unknown <candandilovan@gmail.com>
2025-06-18 10:43:16 +0200

    merge

--- unique_collector.py
+++ unique_collector.py
@@ -3,14 +3,7 @@ import pandas as pd
 
 def test(src, dstcol):
     df = pd.DataFrame(src)
-    table = df[dstcol]
-    print(table)
-    unique_lst = {dstcol: []}
-    for row in table:
-        if row in unique_lst[dstcol]:
-            pass
-        else:
-            unique_lst[dstcol].append(row)
-    new_df = pd.DataFrame(unique_lst)
+    table = df[dstcol].drop_duplicates().reset_index(drop=True)
+    new_df = pd.DataFrame({dstcol: table})
     print(new_df)
     return new_df
