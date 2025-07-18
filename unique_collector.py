2e64bcd67cdbf5b84196ad56d829823655d1b52c
unknown <candandilovan@gmail.com>
2025-07-18 17:31:33 +0200

    debug

--- unique_collector.py
+++ unique_collector.py
@@ -11,9 +11,9 @@ def oto_unique(src, dstcol):
 def otm_unique(src, dstcol):
     df = pd.DataFrame(src)
     unique_lst = new_Dict(dstcol)
-    for y in range(len(dstcol)):
-        for x in range(len(df[dstcol[y]])):
-            if df[dstcol[y]][x] not in unique_lst[dstcol[y]]:
+    for x in range(len(df[dstcol[0]])):
+        if df[dstcol[0]][x] not in unique_lst[dstcol[0]]:
+            for y in range(len(dstcol)):
                 unique_lst[dstcol[y]].append(df[dstcol[y]][x])
     print(unique_lst)
     new_df = pd.DataFrame.from_dict(unique_lst)
